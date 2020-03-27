7ad860ddd3885a43c1452188937ff72cfc276392
chpoullet <59974805+chpoullet@users.noreply.github.com>
2020-03-27 06:46:40 +0000

    Update Policyfile.rb

--- Policyfile.rb
+++ Policyfile.rb
@@ -10,7 +10,7 @@ name 'beatsapp2_cookbook'
 default_source :supermarket
 
 # run_list: chef-client will run these recipes in the order specified.
-run_list 'beatsapp_cookbook2::default'
+run_list 'beatsapp2_cookbook::default'
 
 # Specify a custom source for a single cookbook:
 cookbook 'beatsapp2_cookbook', path: '.'
